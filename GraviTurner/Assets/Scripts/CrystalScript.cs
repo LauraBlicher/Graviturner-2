2307f085266f371e03020c50934f6e4b46d0d2d4
LauraBlicher <Lblicher@gmail.com>
2019-01-18 13:48:42 +0100

    crystal break works!

---
+++ GraviTurner/Assets/Scripts/CrystalScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalScript : MonoBehaviour {
+
+	public ParticleSystem shards, sparkles;
+	private GameObject player;
+	private AudioSource audioSource; 
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BreakCrystal()
+	{
+		shards.Play ();
+		sparkles.Play ();
+		GetComponent<Animator> ().SetBool ("isBreak", true);
+		audioSource.Play (); 
+	
+	}
+
+	public void CrystalBroke()
+	{
+		player.SendMessage("CrystalBroke");
+	}
+}
